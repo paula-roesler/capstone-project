5185095fd968d7c5522813e689474055622d7a20
Paula Rösler <paularoesler@web.de>
2021-03-08 15:55:40 +0100

    Add storybook AddPlayerForm.stories.js

---
+++ src/components/AddPlayerForm/AddPlayerForm.js
@@ -0,0 +1,10 @@
+// import PropTypes from 'prop-types'
+
+function AddPlayerForm() {
+  return <h2>Add Player:</h2>
+}
+
+export default AddPlayerForm
+
+// AddPlayerForm.PropTypes = {}
+// AddPlayerForm.defaultProps = {}
---
+++ src/components/AddPlayerForm/AddPlayerForm.spec.js
@@ -0,0 +1,2 @@
+import { screen, render } from '@testing-library/react'
+import AddPlayerForm from './AddPlayerForm'
