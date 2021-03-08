75ebb88ec68a5e16d6a293f95bcca598809e340d
Paula Rösler <paularoesler@web.de>
2021-03-08 16:09:22 +0100

    AddPlayerForm basics

--- src/components/AddPlayerForm/AddPlayerForm.js
+++ src/components/AddPlayerForm/AddPlayerForm.js
@@ -1,10 +1,17 @@
+import styled from 'styled-components/macro'
 // import PropTypes from 'prop-types'
 
-function AddPlayerForm() {
-  return <h2>Add Player:</h2>
+export default function AddPlayerForm() {
+  return (
+    <>
+      <h2>Add Player:</h2>
+      <label text="AddPlayer">
+        <input placeholder="Add Player"></input>
+      </label>
+      <button>add</button>
+    </>
+  )
 }
-
-export default AddPlayerForm
 
 // AddPlayerForm.PropTypes = {}
 // AddPlayerForm.defaultProps = {}
