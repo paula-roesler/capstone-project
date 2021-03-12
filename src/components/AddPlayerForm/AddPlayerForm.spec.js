742c743e054301f0609f9e87fa63975ac1a9b52a
Paula Rösler <paularoesler@web.de>
2021-03-12 16:50:46 +0100

    Add all corrections

--- src/components/AddPlayerForm/AddPlayerForm.spec.js
+++ src/components/AddPlayerForm/AddPlayerForm.spec.js
@@ -18,8 +18,8 @@ describe('AddPlayerForm', () => {
     it('applies the name of a player to the submit callback', () => {
       const { queryByTitle } = render(<AddPlayerForm />)
       const input = queryByTitle('addPlayerInput')
-      fireEvent.change(input, { target: { value: 'Sue' } })
-      expect(input.value).toBe('Sue')
+      fireEvent.change(input, { target: { value: 'Izzy' } })
+      expect(input.value).toBe('Izzy')
 
       screen.debug()
     })
