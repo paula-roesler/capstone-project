6f54ae8dad0a722e758c73bf3f407a2574655bb1
Paula Rösler <paularoesler@web.de>
2021-04-03 19:17:16 +0200

    Add set url to icon from weather api

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -5,11 +5,20 @@ export default function Weather(weather) {
 
   return (
     <WeatherForecast>
-      <div>City: {weather['weather']['city']['name']}</div>
-      <div>Temperature: {weather['weather']['list'][0]['main']['temp']}</div>
+      <div>Hamburg Wetter: {weather['weather']['cnt']}</div>
+      <div>Hamburg Wetter: {weather['weather']['cod']}</div>
+
+      <div>Stadt Name: {weather['weather']['city']['name']}</div>
+      <div>Country: {weather['weather']['city']['country']}</div>
+      <div>temperature: {weather['weather']['list'][0]['main']['temp']}</div>
       <div>
-        Feels like: {weather['weather']['list'][0]['main']['feels_like']}
+        feels like: {weather['weather']['list'][0]['main']['feels_like']}
       </div>
+      <div>icon: {weather['weather']['list'][0]['weather'][0]['icon']}</div>
+      <img
+        src={`https://openweathermap.org/img/wn/${weather['weather']['list'][0]['weather'][0]['icon']}.png`}
+        alt=""
+      />
     </WeatherForecast>
   )
 }
