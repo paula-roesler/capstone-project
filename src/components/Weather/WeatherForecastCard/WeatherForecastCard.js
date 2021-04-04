bd4dbb77fbb7982ae4c206502cfd3724a1a42541
Paula Rösler <paularoesler@web.de>
2021-04-04 09:21:26 +0200

    Add second day to weather list

--- src/components/Weather/WeatherForecastCard/WeatherForecastCard.js
+++ src/components/Weather/WeatherForecastCard/WeatherForecastCard.js
@@ -3,9 +3,14 @@ import styled from 'styled-components/macro'
 export default function WeatherForecastCard(weather, date) {
   const date1 = new Date(weather['weather']['weather']['list'][0]['dt'] * 1000)
   const day1 = date1.toDateString()
+  const date2 = new Date(weather['weather']['weather']['list'][9]['dt'] * 1000)
+  const day2 = date2.toDateString()
   //   const dayx = date1.toLocaleDateString()
-  const degree = weather['weather']['weather']['list'][0]['main']['temp']
-  const removedDecimal = Math.trunc(degree)
+  const degree1 = weather['weather']['weather']['list'][0]['main']['temp']
+  const degDay1 = Math.trunc(degree1)
+
+  const degree2 = weather['weather']['weather']['list'][9]['main']['temp']
+  const degDay2 = Math.trunc(degree2)
 
   console.log(weather)
 
@@ -14,13 +19,17 @@ export default function WeatherForecastCard(weather, date) {
       <h2>Hamburg weather</h2>
       <ForecastDay>
         <h4>{day1}</h4>
-        <ForecastIcon
-          src={`https://openweathermap.org/img/wn/${weather['weather']['weather']['list'][0]['weather'][0]['icon']}.png`}
-          alt=""
-        />
-
-        <ForecastDegree>{removedDecimal} °C</ForecastDegree>
+        <ForecastHead>
+          <ForecastDegree>{degDay1} °C</ForecastDegree>
+          <ForecastIcon>
+            <img
+              src={`https://openweathermap.org/img/wn/${weather['weather']['weather']['list'][0]['weather'][0]['icon']}.png`}
+              alt=""
+            />
+          </ForecastIcon>
+        </ForecastHead>
         <ForecastDescription>
+          Main:{' '}
           {
             weather['weather']['weather']['list'][0]['weather'][0][
               'description'
@@ -33,26 +42,60 @@ export default function WeatherForecastCard(weather, date) {
           {weather['weather']['weather']['list'][0]['main']['humidity']}%
         </ForecastDescription>
       </ForecastDay>
+      <ForecastDay>
+        <h4>{day2}</h4>
+        <ForecastHead>
+          <ForecastDegree>{degDay2} °C</ForecastDegree>
+          <ForecastIcon>
+            <img
+              src={`https://openweathermap.org/img/wn/${weather['weather']['weather']['list'][9]['weather'][0]['icon']}.png`}
+              alt=""
+            />
+          </ForecastIcon>
+        </ForecastHead>
+        <ForecastDescription>
+          Main:{' '}
+          {
+            weather['weather']['weather']['list'][9]['weather'][0][
+              'description'
+            ]
+          }
+          <br></br>
+          Wind: {weather['weather']['weather']['list'][9]['wind']['speed']} km/h
+          <br></br>
+          Humidity:{' '}
+          {weather['weather']['weather']['list'][9]['main']['humidity']}%
+        </ForecastDescription>
+      </ForecastDay>
     </ForecastWrapper>
   )
 }
+export const ForecastHead = styled.div`
+  display: flex;
+  justify-items: space-between;
+`
 
+export const ForecastIcon = styled.div`
+  width: 60px;
+  height: 60px;
+`
 export const ForecastDegree = styled.p`
   font-size: 150%;
+  margin: 0;
+  width: 60px;
+  height: 60px;
 `
-export const ForecastDescription = styled.p``
-
-export const ForecastIcon = styled.img`
-  position: absolute;
-  top: 12px;
-  right: 30px;
+export const ForecastDescription = styled.div`
+  margin: 0;
 `
 
 export const ForecastDay = styled.div`
-  padding: 1px 20px;
+  padding: 1px 20px 20px;
   border-radius: 8px;
   box-shadow: 0 0 10px 4px rgba(0, 0, 0, 0.2);
-  position: relative;
 `
 
-export const ForecastWrapper = styled.div``
+export const ForecastWrapper = styled.div`
+  display: grid;
+  gap: 20px;
+`
