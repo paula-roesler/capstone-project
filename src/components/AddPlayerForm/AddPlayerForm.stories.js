3ca0c3eef5a504aa4141c4dc2914b6c63e43c35b
Paula Rösler <paularoesler@icloud.com>
2021-04-09 07:08:32 +0200

    Add stb controls to main.js

--- src/components/AddPlayerForm/AddPlayerForm.stories.js
+++ src/components/AddPlayerForm/AddPlayerForm.stories.js
@@ -1,9 +1,14 @@
 import AddPlayerForm from './AddPlayerForm'
-// import { action } from '@storybook/addon-actions'
+import { action } from '@storybook/addon-actions'
 
 export default {
   title: 'AddPlayerForm',
   component: 'AdAddPlayerForm',
+  parameters: {
+    actions: {
+      handles: ['mouseover', 'click .btn'],
+    },
+  },
 }
 
 export const defaultAddPlayerForm = () => <AddPlayerForm />
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,8 +1,10 @@
-import { Button } from './Button'
+import Button from './Button'
+import { action } from '@storybook/addon-actions'
 
 export default {
   title: 'Button',
   component: 'Button',
+  parameters: { actions: { argTypesRegex: '^on.*' } },
 }
 
 export const defaultButton = () => <Button>test</Button>
