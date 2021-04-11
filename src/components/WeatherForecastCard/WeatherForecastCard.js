59e92514739edd5b98e784ecbf615b8b5fcf0552
Paula Rösler <paularoesler@icloud.com>
2021-04-11 16:47:07 +0200

    Rearrange weatherforecast cards

--- src/components/WeatherForecastCard/WeatherForecastCard.js
+++ src/components/WeatherForecastCard/WeatherForecastCard.js
@@ -7,28 +7,29 @@ export default function WeatherForecastCard(weather) {
   return (
     <ForecastWrapper>
       <ForecastDay>
-        <ForecastDate>{new Date(weather['weather']['weather']['list'][currentDay]['dt'] * 1000).toDateString()}</ForecastDate>
-        <ForecastHead>
+            <ForecastDescription>
+            <ForecastDate>{new Date(weather['weather']['weather']['list'][currentDay]['dt'] * 1000).toDateString()}</ForecastDate>
+              {
+                weather['weather']['weather']['list'][currentDay]['weather'][0][
+                  'description'
+                ]
+              }
+              <br></br>
+              Wind: {weather['weather']['weather']['list'][currentDay]['wind']['speed']} km/h
+              <br></br>
+              Humidity:{' '}
+              {weather['weather']['weather']['list'][currentDay]['main']['humidity']}%
+            </ForecastDescription>
+        <DegreeAndSymbol>
           <ForecastDegree>{Math.trunc(weather['weather']['weather']['list'][currentDay]['main']['temp'])} °C</ForecastDegree>
           <ForecastIcon>
             <img
               src={`src=./../images/weathericons/${weather['weather']['weather']['list'][currentDay]['weather'][0]['icon']}.svg`}
               alt=""
-            />
+              />
           </ForecastIcon>
-        </ForecastHead>
-        <ForecastDescription>
-          {
-            weather['weather']['weather']['list'][currentDay]['weather'][0][
-              'description'
-            ]
-          }
-          <br></br>
-          Wind: {weather['weather']['weather']['list'][currentDay]['wind']['speed']} km/h
-          <br></br>
-          Humidity:{' '}
-          {weather['weather']['weather']['list'][currentDay]['main']['humidity']}%
-        </ForecastDescription>
+        </DegreeAndSymbol>
+
       </ForecastDay>
     </ForecastWrapper>
   )
@@ -36,39 +37,37 @@ export default function WeatherForecastCard(weather) {
 
 
 
+export const DegreeAndSymbol = styled.div`
+text-align: center;
+`
+export const ForecastIcon = styled.div`
+  width: 70px;
+  height: 20px;
+  padding: 7px;
+  margin: 0 auto;
+`
+export const ForecastDegree = styled.div`
+  font-size: 150%;
+  margin: 0;
+  padding-top: 15px;
+  width: 80px;
+  height: 60px;
+`
 export const ForecastDate = styled.h4`
   color: var(--primary);
   margin-bottom: 0;
 `
-
-export const ForecastHead = styled.div`
-  display: flex;
-  justify-items: space-between;
-`
-
-export const ForecastIcon = styled.div`
-  width: 60px;
-  height: 20px;
-  padding: 7px;
-`
-export const ForecastDegree = styled.p`
-  font-size: 150%;
-  margin: 0;
-  padding-top: 10px;
-  width: 80px;
-  height: 60px;
-`
 export const ForecastDescription = styled.div`
   margin: 0;
 `
-
 export const ForecastDay = styled.div`
+  display: flex;
+  justify-content: space-between;
   padding: 1px 20px 20px;
   border-radius: 8px;
   box-shadow: 0 0 10px 4px rgba(0, 0, 0, 0.2);
   background-color: var(--background);
 `
-
 export const ForecastWrapper = styled.div`
   display: grid;
   gap: 20px;
