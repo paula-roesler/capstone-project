8eb226181b3c45fe8bc2c861b6b8ef2cb8ad244c
Paula Rösler <paularoesler@icloud.com>
2021-04-11 08:59:42 +0200

    Add cypress tests to AddPlayerForm

--- test/e2e/addPlayerForm.spec.js
+++ test/e2e/addPlayerForm.spec.js
@@ -14,5 +14,22 @@ describe('<AddPlayerForm /> component', () => {
     cy.get('[data-testid=inputPlayer]').type('Jim').type('{enter}')
   })
 
-  
+  it('should focus the input after the user clicks the add button', () => {
+    cy.get('input[name="playerName"]').focus()
+  })
+
+  it('should show the start game button when the user added at least two players', () => {
+    cy.get('[data-testid=inputPlayer]').type('Sue').type('{enter}')
+    cy.get('[data-testid=inputPlayer]').type('Jim').type('{enter}')
+    cy.get('[href="/one"]').should('be.visible')
+  })
+
+  it('should change the placeholder text when 4 players are added and disable the add button', () => {
+    cy.get('[data-testid=inputPlayer]').type('Sue').type('{enter}')
+    cy.get('[data-testid=inputPlayer]').type('Jim').type('{enter}')
+    cy.get('[data-testid=inputPlayer]').type('Joe').type('{enter}')
+    cy.get('[data-testid=inputPlayer]').type('Izzy').type('{enter}')
+    cy.get('input[placeholder="Your flight has reached the max. of 4 players"]').should('exist')
+    cy.get('button[title="addPlayerButton"]').should('be.disabled')
+  })
 })
